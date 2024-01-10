c319719d5ec0d3aeeab555a78919a4a72ddb27c0
Sabrina Giancaspro <143004633+SabrinaGiancaspr@users.noreply.github.com>
2024-01-10 13:57:27 +0100

    Initial commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+
+
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $types = Type::orderBy('name', 'ASC')->get();
+        return view('admin.projects.create', compact('types'));
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255|string|unique:projects',
+            'type_id' => 'nullable|exists:types,id'
+        ]);
+
+        $data = $request->all();
+        $data['link'] = 'https://github.com/SabrinaGiancaspr/'. Str::slug($data['name']);
+        $new_project = Project::create($data);
+        return redirect()->route('admin.projects.show', $new_project);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project)
+    {
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::orderBy('name', 'ASC')->get();
+        return view ('admin.projects.edit', compact('project', 'types'));
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Project $project)
+    {
+        $request->validate([
+            'name' => ['required',' max:255', 'string', Rule::unique('projects')->ignore($project->id)],
+            'type_id' => 'nullable|exists:types,id'
+        ]);
+        
+        $data = $request->all();
+        $data['link'] = 'https://github.com/SabrinaGiancaspr/'. Str::slug($data['name']);
+        $project->update($data);
+        return redirect()->route('admin.projects.index', $project);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return redirect()->route('admin.projects.index');
+    }
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker)
+    {
+        $types = Type::all();
+        $ids = $types->pluck('id');
+
+        for($i= 0; $i < 10; $i++){
+            $new_project = new Project();
+            $new_project->name = $faker->sentence(5);
+            $new_project->description = $faker->text(500);
+            $new_project->project_type = $faker->randomElement(['Front-End', 'Back-End', 'Full-stack']);
+            $new_project->link = 'https://github.com/SabrinaGiancaspr/'. Str::slug($new_project->name);
+            $new_project->project_status = $faker->randomElement(['in progress', 'done']);
+            $new_project->type_id = $faker->optional()->randomElement($ids);
+            $new_project->save();
+        }
+    }
+}
